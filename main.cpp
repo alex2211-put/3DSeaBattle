6520df459f54ea85e4a7421ec287738af3bcc374
alik <putin.al@phystech.edu>
2020-03-12 00:59:32 +0300

    сделал куб прозрачным, следующий шаг - интерактив

--- main.cpp
+++ main.cpp
@@ -6,11 +6,16 @@ double rotate_x=0;
 double sizeCell = 0.5/10;
 double  stepCell = 0.1; //шаг при определении
 double quantityCell = 0.8; //количестве кубиков
+double transparency = 0.3; //прозрачность кубиков
 void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     glRotatef( rotate_x, 1.0, 0.0, 0.0 );
     glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+
+    glEnable(GL_ALPHA_TEST); //включаем прозрачность
+    glEnable(GL_BLEND);  //разрешаем мешать цвета
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  //устанавдиваем уровень прозрачности - пока до конца не разобрался
 
     for (float i = 0; i < quantityCell; i+=stepCell)
     {
@@ -19,51 +24,51 @@ void display(){
             for (float y = 0; y < quantityCell; y+=stepCell)
             {
                 glBegin(GL_POLYGON);
-                glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  sizeCell + i, -sizeCell + j, -sizeCell + y);
-                glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  sizeCell + i,  sizeCell + j, -sizeCell + y);
-                glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i,  sizeCell + j, -sizeCell + y);
-                glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i, -sizeCell + j, -sizeCell + y);
+                glColor4f( 1.0, 0.0, 0.0, transparency );     glVertex3f(  sizeCell + i, -sizeCell + j, -sizeCell + y);
+                glColor4f( 0.0, 1.0, 0.0, transparency );     glVertex3f(  sizeCell + i,  sizeCell + j, -sizeCell + y);
+                glColor4f( 0.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i,  sizeCell + j, -sizeCell + y);
+                glColor4f( 1.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i, -sizeCell + j, -sizeCell + y);
                 glEnd();
 
                 glBegin(GL_POLYGON);
-                glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  sizeCell + i, -sizeCell + j, sizeCell + y);
-                glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  sizeCell + i,  sizeCell + j, sizeCell + y);
-                glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i,  sizeCell + j, sizeCell + y);
-                glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i, -sizeCell + j, sizeCell + y);
+                glColor4f( 1.0, 0.0, 0.0, transparency );     glVertex3f(  sizeCell + i, -sizeCell + j, sizeCell + y);
+                glColor4f( 0.0, 1.0, 0.0, transparency );     glVertex3f(  sizeCell + i,  sizeCell + j, sizeCell + y);
+                glColor4f( 0.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i,  sizeCell + j, sizeCell + y);
+                glColor4f( 1.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i, -sizeCell + j, sizeCell + y);
                 glEnd();
 
                 glBegin(GL_POLYGON);
-                glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  sizeCell + i, -sizeCell + j, -sizeCell + y);
-                glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  sizeCell + i,  sizeCell + j, -sizeCell + y);
-                glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( sizeCell + i,  sizeCell + j, sizeCell + y);
-                glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( sizeCell + i, -sizeCell + j, sizeCell + y);
+                glColor4f( 1.0, 0.0, 0.0, transparency );     glVertex3f(  sizeCell + i, -sizeCell + j, -sizeCell + y);
+                glColor4f( 0.0, 1.0, 0.0, transparency );     glVertex3f(  sizeCell + i,  sizeCell + j, -sizeCell + y);
+                glColor4f( 0.0, 0.0, 1.0, transparency );     glVertex3f( sizeCell + i,  sizeCell + j, sizeCell + y);
+                glColor4f( 1.0, 0.0, 1.0, transparency );     glVertex3f( sizeCell + i, -sizeCell + j, sizeCell + y);
                 glEnd();
 
                 glBegin(GL_POLYGON);
-                glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  -sizeCell + i, -sizeCell + j, sizeCell + y);
-                glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  -sizeCell + i,  sizeCell + j, sizeCell + y);
-                glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i,  sizeCell + j, -sizeCell + y);
-                glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i, -sizeCell + j, -sizeCell + y);
+                glColor4f( 1.0, 0.0, 0.0, transparency );     glVertex3f(  -sizeCell + i, -sizeCell + j, sizeCell + y);
+                glColor4f( 0.0, 1.0, 0.0, transparency );     glVertex3f(  -sizeCell + i,  sizeCell + j, sizeCell + y);
+                glColor4f( 0.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i,  sizeCell + j, -sizeCell + y);
+                glColor4f( 1.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i, -sizeCell + j, -sizeCell + y);
                 glEnd();
 
                 glBegin(GL_POLYGON);
-                glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  sizeCell + i, sizeCell + j, sizeCell + y);
-                glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  sizeCell + i,  sizeCell + j, -sizeCell + y);
-                glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i,  sizeCell + j, -sizeCell + y);
-                glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i, sizeCell + j, sizeCell + y);
+                glColor4f( 1.0, 0.0, 0.0, transparency );     glVertex3f(  sizeCell + i, sizeCell + j, sizeCell + y);
+                glColor4f( 0.0, 1.0, 0.0, transparency );     glVertex3f(  sizeCell + i,  sizeCell + j, -sizeCell + y);
+                glColor4f( 0.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i,  sizeCell + j, -sizeCell + y);
+                glColor4f( 1.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i, sizeCell + j, sizeCell + y);
                 glEnd();
 
                 glBegin(GL_POLYGON);
-                glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  sizeCell + i, -sizeCell + j, -sizeCell + y);
-                glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  sizeCell + i,  -sizeCell + j, sizeCell + y);
-                glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i,  -sizeCell + j, sizeCell + y);
-                glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -sizeCell + i, -sizeCell + j, -sizeCell + y);
+                glColor4f( 1.0, 0.0, 0.0, transparency );     glVertex3f(  sizeCell + i, -sizeCell + j, -sizeCell + y);
+                glColor4f( 0.0, 1.0, 0.0, transparency );     glVertex3f(  sizeCell + i,  -sizeCell + j, sizeCell + y);
+                glColor4f( 0.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i,  -sizeCell + j, sizeCell + y);
+                glColor4f( 1.0, 0.0, 1.0, transparency );     glVertex3f( -sizeCell + i, -sizeCell + j, -sizeCell + y);
                 glEnd();
             }
         }
     }
 
-
+    //glDisable(GL_BLEND);     выключаем прозрачность
     glFlush();
     glutSwapBuffers();
 }
